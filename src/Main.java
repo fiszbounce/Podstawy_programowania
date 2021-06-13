13db1c8caa6cf22e4647a056622d0f6e4f99f92b
Piotr Fiszbach <fiszbounce@gmail.com>
2021-06-13 14:27:55 +0100

    dzialania na liczbach

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
 public class Main {
     public static void main (String args[]) {
-        String text = "pisz";
-        String text2 = new String("ladnie");
-        String text3 = ("ok");
-        text.length();
-        System.out.println(text.length());
-        System.out.println(text.indexOf('s'));
-        System.out.println(text.trim());
-        System.out.println(text.substring(0,2));
-        text.toCharArray()
-
+        int a;
+        int b;
+        a = 30;
+        b = 10;
+        System.out.println(a+b);
+        System.out.println(a*b);
+        System.out.println((double)a/b);
+        System.out.println(a%b);
 
     }
 }
